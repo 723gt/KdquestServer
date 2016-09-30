01248dd0a9b7e126915ec108dc0bc6d4e7b6f3cd
723 <rb.natsumi@gmail.com>
2016-09-30 09:56:55 +0900

    Copyright writing

--- server.rb
+++ server.rb
@@ -1,8 +1,15 @@
 #!/urs/bin/env ruby
 
+#UDP server in the KD Quest project
+#Git repo: https://bitbucket.org/723/sfserver-2016
+#Copyright (c) 2016 kobedenshi.ac.jp Procon Club All Rights Reserved. 
+#Create by Show Nakamura & Natsumi Yoshioka  
+
+#mylib
 require "./lib/bdctrl"
 require "./lib/jsonmake"
 
+#object create
 jsonmake = Jsonmake.new
 dbctrl = Dbctrl.new(jsonmake)
 
